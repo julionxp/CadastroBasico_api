bec701075bde05f34e840129cef8e6d4d440a8e6
julio <juliondiniz@gmail.com>
2025-11-05 16:41:35 -0300

    Básico Cadastro 1.2

--- src/main/java/com/CadastroBasico/api/controller/CadastroController.java
+++ src/main/java/com/CadastroBasico/api/controller/CadastroController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.CadastroBasico.api.model.Cadastro;
@@ -16,26 +17,74 @@ public class CadastroController {
     @Autowired
     private CadastroRepository cadastroRepository;
 
-    
-    
+
+
+
+
+    @GetMapping("/home")
+    public String mostrarHome(Model model) {
+        model.addAttribute("cadastro", new Cadastro());
+        return "home";
+    }
+
+    // @GetMapping("/final")
+    // public String mostrarFinal() {
+    //     return "final";
+    // }
+
+
+
+
+
+
     @GetMapping("/cadastro")
     public String mostrarCadastro(Model model) {
         model.addAttribute("cadastro", new Cadastro());
         return "cadastro";
     }
-
-    @PostMapping("/usuario")
+    
+    @PostMapping("/cadastro")
     public String salvarCadstro(@ModelAttribute Cadastro usuario) {
         cadastroRepository.save(usuario);
-        return "redirect:/successfulCadastro";
+        return "redirect:/login";
     }
+
 
 
 
     
 
+    @GetMapping("/login")
+    public String mostrarLogin(Model model) {
+        model.addAttribute("cadastro", new Cadastro());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String fazerLogin(@ModelAttribute Cadastro cadastro, Model model) {
+
+        Cadastro usuario = cadastroRepository.findByEmailAndSenha(
+            cadastro.getEmail(),
+            cadastro.getSenha()
+        );
+
+        if (usuario != null) {
+            return "redirect:/final";
+        
+        } else {
+            model.addAttribute("erro", "Email ou senha incorretos!");
+            return "login";
+        }
+    }
 
 
+
+    @GetMapping("/final")
+    public String mostrarAgradecimento(Model model) {
+        model.addAttribute("mensagem", "Login efetuado com sucesso. Obrigado por acessar!");
+        return "final";
+    
+    }
 
 
 }
